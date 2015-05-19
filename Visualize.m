b425ae1080806f0bf078d6996d4331145e0b07af
Rob <rm7536@rit.edu>
2015-05-18 21:31:21 -0400

    updated visualizer

--- Visualize.m
+++ Visualize.m
@@ -15,22 +15,21 @@ actual = [1 11 1 5 1 6 6 2 3 5 12 2 6 2 7 8 6 9 9 10 ...
     3 1 4 9 1 5 5 9 3 12 1 9 12 6 8 3 8 1 12 2 ];
 
 addpath code
-% need square images for the display program
-resizing = [100 100];
+IMCHOICE = 1;
+data = load(fullfile('data', 'leap-binary-large', 'imdb.mat')) ;
+imshow(data.images.data(:,:,:,IMCHOICE));
 
 input('Visualizing a subset of the data. Press Enter')
 clf ;
-data = load(fullfile('data','leap-binary-large', 'imdb.mat')) ;
 % look at some random data
 num = 20 ;
 rows = [] ;
 for i=1:num,
-    rows = [rows ; reshape(imresize(data.images.data(:,:,:,i), resizing) ,1,[]) ] ;
+    rows = [rows ; reshape(data.images.data(:,:,:,i), 1, []) ] ;
 end;
-displayData(rows) ;
+displayData( rows, size(data.images.data, 2) ) ;
 
 input('Training the classifier. Press Enter')
-clf ; 
 x = cnn_leap ;
 
 input('Visualizing the first layer of filters. Press Enter')
@@ -38,24 +37,67 @@ clf ;
 out = gather(x.layers{1, 1}.filters) ;
 rows = [] ;
 for i=1:size(out, 4),
-    rows = [rows ; reshape(out(:,:,1,i).',1,[]) ] ;
+    rows = [rows ; reshape(out(:,:,1,i),1,[]) ] ;
 end;
 displayData(rows) ;
+
+input('Applying convolution to image. Press Enter')
+clf ;
+l = x.layers{1} ;
+conv = vl_nnconv(data.images.data(:,:,:,IMCHOICE), gather(l.filters), gather(l.biases));
+rows = [] ;
+for i=1:size(conv, 3),
+    rows = [rows ; reshape(conv(:,:,i), 1, []) ] ;
+end;
+displayData(rows, size(conv, 2)) ;
 
 input('Visualizing the second layer of filters. Press Enter')
 clf ;
 out = gather(x.layers{1, 3}.filters) ;
 rows = [] ;
 for i=1:size(out, 4),
-    rows = [rows ; reshape(out(:,:,1,i).',1,[]) ] ;
+    rows = [rows ; reshape(out(:,:,1,i),1,[]) ] ;
 end;
 displayData(rows) ;
+
+input('Applying pool and conv to image choice. Press Enter')
+clf ;
+l = x.layers{2} ;
+conv = vl_nnpool(conv, l.pool, 'pad', l.pad, 'stride', l.stride, 'method', l.method) ;
+l = x.layers{3} ;
+conv = vl_nnconv(conv, gather(l.filters), gather(l.biases)) ; 
+rows = [] ;
+for i=1:size(conv, 3),
+    rows = [rows ; reshape(conv(:,:,i), 1, []) ] ;
+end;
+displayData(rows, size(conv, 2)) ;
+
+input('Visualizing the third layer of filters. Press Enter')
+clf ;
+out = gather(x.layers{1, 5}.filters) ;
+rows = [] ;
+for i=1:size(out, 4),
+    rows = [rows ; reshape(out(:,:,1,i),1,[]) ] ;
+end;
+displayData(rows, size(out,2)) ;
+
+input('Applying next pool and conv to image choice. Press Enter')
+clf ;
+l = x.layers{4} ;
+conv = vl_nnpool(conv, l.pool, 'pad',l.pad, 'stride', l.stride, 'method', l.method);
+l = x.layers{5} ;
+conv = vl_nnconv(conv, gather(l.filters), gather(l.biases)) ; 
+rows = [] ;
+for i=1:size(conv, 3),
+    rows = [rows ; reshape(conv(:,:,i), 1, []) ] ;
+end;
+displayData(rows, size(conv, 2) ) ;
 
 input('Visualizing the errors. Press Enter')
 clf ;
 val = data.images.data(:,:,:,data.images.set==3);
 rows = [] ;
 for elm = wrong
-    rows = [rows ; reshape(imresize(val(:,:,1,elm), resizing).',1,[]) ] ;
+    rows = [rows ; reshape(val(:,:,1,elm), 1, []) ] ;
 end;
-displayData(rows) ;
+displayData(rows, size(val,2) ) ;
